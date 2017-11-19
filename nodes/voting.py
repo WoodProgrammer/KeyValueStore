99d2bbe2f451a2be56921b29a40b2d253ffecaaf
woodprogrammer <asddeneme3@gmail.com>
2017-11-20 00:39:57 +0200

    RabbitMQ Messaging methods written

--- nodes/voting.py
+++ nodes/voting.py
@@ -1,25 +1,52 @@
-import socket
+###rabbitmq handler
+
+#### 1-) request vote channel (host_ip)
+#### 2-) send vote channel send_vote (host_ip,dest_ip)
+
+import pika
+
+
 class Voting:
+    def __init__(self):
+        self.voting_flag = 0
+        self.credentials = pika.PlainCredentials('user', 'password')
+        self.parameters = pika.ConnectionParameters('localhost',
+                                            5672,
+                                            '/',
+                                            self.credentials)
+                        
+        self.connection = pika.BlockingConnection(self.parameters)
+        self.channel = self.connection.channel()    
+        self.channel.queue_declare(queue='election') ##send the vote 
+        self.channel.queue_declare(queue='receive_') ## request the vote s
+  
+    def request_vote(self):
+        self.channel.basic_publish(exchange='',
+                      routing_key='receive_',
+                      body='Hello World!')
 
-    def __init___(self):
-        self.sock = socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
 
-    def send_vote(self,candidate_ip,node_obj):
-
-        ###send to candidate 
-        server_address = ('{}'.format(candidate_ip),9090)
-        self.sock.bind(server_address)
-        self.sock.listen(1)
-        while True:
-            if node_obj.status == 0:
-                connection,client_addr = self.sock.accept()
-            elif node_obj.status == 1:
-                break
+    def count_voting_result(self): ##consumer
+        self.channel.basic_consume(self.send_vote,
+                      queue='election',
+                      no_ack=True)
 
 
-    def request_vote(self,node_obj):
-        ##broad_casting 
+    def send_vote(self,ch, method, properties, body):## it send vote
         
+        self.channel.basic_publish(exchange='',
+                      routing_key='election',
+                      body='{}'.format(body))
+
+        print(body)
 
 
-        pass
+
+
+
+
+    def receive_vote_req(self): ### it receive request.
+
+        self.channel.basic_consume(self.send_vote,
+                      queue='receive_',
+                      no_ack=True)
