61efb92fce64cb824eb1fd962312ab97215c8d5d
woodprogrammer <asddeneme3@gmail.com>
2017-12-10 01:39:56 +0200

    New Hash function added

--- bloom_filter/main.cpp
+++ bloom_filter/main.cpp
@@ -1,7 +1,10 @@
 #include "hashing.h"
 #include <iostream>
 #define N 10000000000
-          
+#define A 54059 /* a prime */
+#define B 76963 /* another prime */
+#define C 86969 /* yet another prime */
+#define FIRSTH 37 /* also prime */       
     
 bool *sieve = new bool[N];
 int hash_func_1(char *key)
@@ -19,17 +22,41 @@ int hash_func_1(char *key)
     return hash%(6753);
 }
 
-bool get_from_bloom ( char *instance) {
-    unsigned index = hash_func_1(instance);
-    return sieve[index];
+unsigned hash_func_2(const char* s)
+{
+   unsigned h = FIRSTH;
+   while (*s) {
+     h = (h * A) ^ (s[0] * B);
+     s++;
+   }
+   return h;
+}
+
+
+
+bool get_from_bloom ( char *str_instance) {
+    unsigned index = hash_func_1(str_instance);
+    unsigned index2 = hash_func_2(str_instance);
+
+    if (sieve[index] == true && sieve[index2] == true){
+        return true;
+    }else{
+        return false;
+    }
 
 }
-void set_to_bloom ( char *instance) {
-    unsigned index = hash_func_1(instance);
+void set_to_bloom ( char *str_instance) {
+    unsigned index = hash_func_1(str_instance);
+    unsigned index2 = hash_func_2(str_instance);
+
     sieve[index] = true   ;
+    sieve[index2] = true   ;
 }
+
+
+
 
 int main(){
-    set_to_bloom("asdc");
-    std::cout<<get_from_bloom("asdc");
+    set_to_bloom("asdasdasdd");
+    std::cout<<get_from_bloom("asdasdadd");
 }
